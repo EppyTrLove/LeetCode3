5eaec2f6bb858dc594431cd1d1a26f87d021ff70
Nikita Epifantsev <eppy_92@mail.ru>
2023-06-14 22:08:07 +0300

    first commit

---
+++ LeetCode3/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public class Solution
+    {
+        public static void Main(string[] args)
+        {
+            var num = Console.ReadLine();
+            Console.WriteLine(CheckPerfectNumber(int.Parse(num))); 
+
+             bool CheckPerfectNumber(int num)
+            {
+                var divisors = new List<int>();
+                for (var i = 1; i < num; i++)
+                    if (num % i == 0)
+                        divisors.Add(i);
+                var sum = divisors.Sum();
+                return sum == num;
+
+            }
+
+        }
+    }
+}
